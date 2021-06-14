b74d0e45d551295161ecc656c11f9dac791050b2
qxang <qxkang@126.com>
2021-06-14 23:20:53 +0800

    test

--- src/MapboxPanel.tsx
+++ src/MapboxPanel.tsx
@@ -47,51 +47,135 @@ export const MapboxPanel: React.FC<Props> = ({ options, data, width, height }) =
         const pointSourceId = 'landslide-point-data'
         const pointID = 'landslide-point';
         const pointLabelID = 'landslide-point-label';
-        const bs_series = data.series[0];
-        const bs_lat = bs_series.fields[1].state.calcs.mean;
-        const bs_lon = bs_series.fields[2].state.calcs.mean;
-        let features = [];
-        let pointFeatures = []
-        pointFeatures.push({
-            'type': 'Feature',
-            'properties': {
-                'label':'基站'
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [bs_lon,bs_lat]
-            }
-        })
-        const typeKeys = "BCDEFGH"
-        for (let i=1;i<data.series.length;i++){
-            let series = data.series[i]
-            let lat = series.fields.find(field => field.name === 'latitude').state.calcs.lastNotNull
-            let lon = series.fields.find(field => field.name === 'longitude').state.calcs.lastNotNull
-            let bl = series.fields.find(field => field.name === 'bl_length').state.calcs.lastNotNull.toFixed(4)
-            features.push({
-                'type': 'Feature',
-                'properties': {
-                    'label':bl+'m'
-                },
-                'geometry': {
-                    'type': 'LineString',
-                    'coordinates': [
-                        [bs_lon,bs_lat],
-                        [lon,lat]
-                    ]
+        const bs_series = data.series[0]['fields'];
+        let res= {}
+        for (let i=0;i<bs_series[0].values['buffer'].length;i++){
+            let client_id = bs_series[4].values["buffer"][i]
+            if (client_id in res){
+                if (bs_series[1].values["buffer"][i]>0){
+                    res[client_id]['lat'].push(bs_series[1].values["buffer"][i]);   
                 }
-            })
+                if (bs_series[2].values["buffer"][i]>0){
+                    res[client_id]['lon'].push(bs_series[2].values["buffer"][i]);   
+                }
+                res[client_id]['model'] = bs_series[3].values["buffer"][i];
+            }else{
+                res[client_id]= {}
+                if (bs_series[1].values["buffer"][i]>0){
+                    res[client_id]['lat'] = [];
+                    res[client_id]['lat'].push(bs_series[1].values["buffer"][i]);    
+                }
+                if (bs_series[2].values["buffer"][i]>0){
+                    res[client_id]['lon'] = [];
+                    res[client_id]['lon'].push(bs_series[2].values["buffer"][i]);    
+                }
+                res[client_id]['model'] = bs_series[3].values["buffer"][i]
+            }
+        }
+        // const bs_lat = bs_series.fields[1].state.calcs.mean;
+        // const bs_lon = bs_series.fields[2].state.calcs.mean;
+        let features = [];
+        let pointFeatures = [];
+        let client_ids = Object.keys(res);
+        client_ids.forEach(client_id=>{
+            let client = res[client_id];
+            let lat = client['lat'][0]
+            let lon = client['lon'][0]
             pointFeatures.push({
                 'type': 'Feature',
                 'properties': {
-                    'label':'辅站'+(typeKeys.indexOf(series.refId)+1)
+                    'label':client['model']=='R'?'移动站'+client_id:'基站'+client_id,
+                    'model':client['model']
                 },
                 'geometry': {
                     'type': 'Point',
-                    'coordinates':[lon,lat]
+                    'coordinates': [lon,lat]
                 }
             })
+        })
+        const typeKeys = "BCDEFGH";
+        const roves = data.series[1].fields;
+        let roves_res = {};
+        for (let i=0;i<roves[0].values["buffer"].length;i++){
+            let lat = roves[1].values["buffer"][i];
+            let lon = roves[2].values["buffer"][i];
+            let sub_num = roves[3].values["buffer"][i];
+            let client_id = roves[4].values["buffer"][i];
+            let bl_length = roves[5].values["buffer"][i];
+            if (client_id in roves_res){
+                if (sub_num in roves_res[client_id] ){
+                    roves_res[client_id][sub_num].push([lon,lat,bl_length])
+                }else{
+                    roves_res[client_id][sub_num]=[]
+                    roves_res[client_id][sub_num].push([lon,lat,bl_length])
+                }
+                
+            }else{
+                roves_res[client_id] = {}
+                roves_res[client_id][sub_num] = []
+                roves_res[client_id][sub_num].push([lon,lat,bl_length])
+            }
         }
+        Object.keys(roves_res).forEach(rov=>{
+            let rove = roves_res[rov];
+            Object.keys(rove).forEach(sub_num=>{
+                let latlons = rove[sub_num][0];
+                features.push({
+                    'type': 'Feature',
+                    'properties': {
+                        'label':latlons[2]+'m'
+                    },
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': [
+                            [res[rov]['lon'][0],res[rov]['lat'][0]],
+                            [latlons[0],latlons[1]]
+                        ]
+                    }
+                })
+                pointFeatures.push({
+                    'type': 'Feature',
+                    'properties': {
+                        'label':'辅站'+(+sub_num+1),
+                        'model':'rove'
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [latlons[0],latlons[1]]
+                    }
+                })
+            })
+        })
+        // }
+        // for (let i=1;i<data.series.length;i++){
+        //     let series = data.series[i]
+        //     let lat = series.fields.find(field => field.name === 'latitude').state.calcs.lastNotNull
+        //     let lon = series.fields.find(field => field.name === 'longitude').state.calcs.lastNotNull
+        //     let bl = series.fields.find(field => field.name === 'bl_length').state.calcs.lastNotNull.toFixed(4)
+        //     features.push({
+                // 'type': 'Feature',
+                // 'properties': {
+                //     'label':bl+'m'
+                // },
+                // 'geometry': {
+                //     'type': 'LineString',
+                //     'coordinates': [
+                //         [bs_lon,bs_lat],
+                //         [lon,lat]
+                //     ]
+                // }
+        //     })
+        //     pointFeatures.push({
+        //         'type': 'Feature',
+        //         'properties': {
+        //             'label':'辅站'+(typeKeys.indexOf(series.refId)+1)
+        //         },
+        //         'geometry': {
+        //             'type': 'Point',
+        //             'coordinates':[lon,lat]
+        //         }
+        //     })
+        // }
         let geojson = {
             'type': 'FeatureCollection',
             'features':features
@@ -100,10 +184,13 @@ export const MapboxPanel: React.FC<Props> = ({ options, data, width, height }) =
             'type': 'FeatureCollection',
             'features':pointFeatures
         }
+        if (pointGeojson.features.length==0)return
+        let tmp = pointGeojson.features[0].geometry.coordinates;
+        let bs_lat = tmp[1];
+        let bs_lon = tmp[0];
         if (map.current.getSource(sourceID)){
             map.current.getSource(sourceID).setData(geojson);
             map.current.getSource(pointSourceId).setData(pointGeojson);
-            
             if (Math.abs(bs_lat-lat)>=0.1 || Math.abs(bs_lon-lng)>=0.1){
                 let bounds =  new mapboxgl.LngLatBounds([bs_lon-0.01, bs_lat-0.01], [bs_lon+0.01, bs_lat+0.01]);
                 setLat(bs_lat.toFixed(4));
@@ -156,7 +243,17 @@ export const MapboxPanel: React.FC<Props> = ({ options, data, width, height }) =
                         'type': 'circle',
                         'source':pointSourceId ,
                         "paint":{
-                            "circle-color":"#51bbd6",
+                            "circle-color":[
+                                'match',
+                                ['get', 'model'],
+                                'B',
+                                '#fbb03b',
+                                'R',
+                                '#223b53',
+                                'rove',
+                                '#e55e5e',
+                                /* other */ '#ccc'
+                            ],
                             'circle-radius':6
                         }
                     })
@@ -178,7 +275,8 @@ export const MapboxPanel: React.FC<Props> = ({ options, data, width, height }) =
                 setLng(bs_lon.toFixed(4))
                 map.current.flyTo({
                     center: [bs_lon,bs_lat],
-                    speed: 0.5
+                    speed: 0.5,
+                    zoom: 15
                 });
             })
         }  
